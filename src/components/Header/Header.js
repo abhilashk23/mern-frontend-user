0817bd54c0207f99cfe9c1c9305e55844a48d29d
abhilashk23 <abhilashgurukul23@gmail.com>
2023-08-13 12:27:48 +0530

    Created frontend for login page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,21 +10,25 @@ function Header() {
                 <p className='p-1 text-2xl font-bold'>LOGO</p>
             </div>
             <div className='flex flex-row justify-between gap-x-5'>
-                <Button variant='contained' disableElevation sx={{
-                    textTransform: 'none',
-                    backgroundColor: 'rgb(243 244 246)',
-                    color: 'rgb(2 6 23)',
-                    '&:hover': {
+                <a href="/login">
+                    <Button variant='contained' disableElevation sx={{
+                        textTransform: 'none',
                         backgroundColor: 'rgb(243 244 246)',
                         color: 'rgb(2 6 23)',
-                    }
-                }}>
-                    <span className='p-1 font-semibold'><a href="/login">Login</a></span>
-                </Button>
+                        '&:hover': {
+                            backgroundColor: 'rgb(243 244 246)',
+                            color: 'rgb(2 6 23)',
+                        }
+                    }}>
+                        <span className='p-1 font-semibold'>Login</span>
+                    </Button>
+                </a>
 
-                <Button variant='contained' disableElevation sx={{ borderRadius: 8, textTransform: 'none' }}>
-                    <span className='p-1 font-semibold'><a href="/register">Sign Up, it's free!</a></span>
-                </Button>
+                <a href="/register">
+                    <Button variant='contained' disableElevation sx={{ borderRadius: 8, textTransform: 'none' }}>
+                        <span className='p-1 font-semibold'>Sign Up, it's free!</span>
+                    </Button>
+                </a>
             </div>
         </div>
     )
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import dummy from '../../images/dummy.png'
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
 
 const LoginForm = ({ onLogin }) => {
     const history = useHistory();
@@ -12,8 +14,9 @@ const LoginForm = ({ onLogin }) => {
         e.preventDefault();
 
         try {
+            // console.log({ username, password });
             const response = await axios.post('http://localhost:5000/users/login', { username, password });
-            console.log(response.data);
+            // console.log(response.data);
             setError('');
             const token = response.data;
 
@@ -23,20 +26,41 @@ const LoginForm = ({ onLogin }) => {
             history.push('/home');
             window.location.reload();
         } catch (error) {
-            setError('Invalid eusername or password');
+            setError('Invalid Credentials!');
         }
     };
 
     return (
-        <div className="login-form">
-            <h2>Login</h2>
-            {error && <div className="error-message">{error}</div>}
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Username" value={username} onChange={(e) => setUserName(e.target.value)} required />
-                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                <button type="submit">Login</button>
-            </form>
-            <p>New Here? Click here to <a href='/register'>Register</a></p>
+        <div className="flex flex-row">
+            <div className='w-1/3 flex items-center justify-center h-screen bg-fuchsia-400'>
+                <img src={dummy} />
+            </div>
+            <div className='w-2/3 flex flex-col p-5'>
+                <div className='grid justify-items-end'>
+                    <p className='p-1 text-2xl font-bold'>LOGO</p>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div className='login-form flex flex-col justify-center items-center'>
+                    <h2 className='text-5xl font-bold'>Welcome back!</h2>
+                    <p className='py-2 text-lg'>Login to your account</p>
+                    <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center w-full my-3 gap-y-3'>
+                        <div className='w-4/5 flex flex-row items-center justify-items-center gap-x-4'>
+                            <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Username" value={username} onChange={(e) => setUserName(e.target.value)} required />
+                        </div>
+                        <div className='w-4/5 flex flex-row items-center justify-items-center gap-x-4'>
+                            <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        </div>
+
+                        {error && <div className="error-message text-red-600">{error}</div>}
+
+                        <button type="submit" disabled={!username || !password} className='w-4/5 bg-fuchsia-600 disabled:bg-neutral-300 text-white disabled:text-neutral-500 p-2.5 rounded-full font-medium text-xl'>Login</button>
+                    </form>
+
+                    <p className='text-md mt-5'>New here? Click here to <a href = "/register" className='text-fuchsia-700 underline decoration-fuchsia-700'>Register</a></p>
+                </div>
+            </div>
         </div>
     );
 };
