26836c0238487088a1a05705e7274e8b21e2e03b
abhilashk23 <abhilashgurukul23@gmail.com>
2023-08-14 12:43:42 +0530

    1. added profile photo update feature, 2. logout feature, 3. Random background color feature until background added

--- src/components/Header2/Header2.js
+++ src/components/Header2/Header2.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import LogoutIcon from '@mui/icons-material/Logout';
 import WallpaperIcon from '@mui/icons-material/Wallpaper';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header2({ user }) {
     const history = useHistory();
@@ -16,11 +17,12 @@ function Header2({ user }) {
     return (
         <div className='w-1/2 border-2 border-black rounded-full bg-white p-5 flex flex-row justify-between'>
             <div>
-                <p className='p-1 text-2xl font-bold'>{user.profileImage ? (<img src={`http://localhost:5000/uploads/${user.profileImage}`} className='rounded-full w-10 h-10' alt="Profile" />) : (<p>{user.name}</p>)}</p>
+                <a href='/update' title="Update profile image"><p className='p-1 text-2xl font-bold'>{user.profileImage ? (<img src={`http://localhost:5000/uploads/${user.profileImage}`} className='rounded-full w-10 h-10' alt="Profile" />) : (<p>{user.name}</p>)}</p></a>
             </div>
             <div className='flex flex-row justify-between items-center gap-x-5'>
-                <a href='/update'><WallpaperIcon /></a>
-                <a href="/login" onClick={handleLogout}><LogoutIcon /></a>
+
+                <a href="" title="Update Background Image"><WallpaperIcon /></a>
+                <a href="/login" onClick={handleLogout} title="Logout"><LogoutIcon /></a>
             </div>
         </div>
     )
